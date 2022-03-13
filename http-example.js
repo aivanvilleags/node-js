1a5fc00a6e29067f7bd24b9b8960d9d04846e4a6
aivan <haivan.villegas@gmail.com>
2022-03-13 09:27:12 -0600

    Missing file

---
+++ http-example.js
@@ -0,0 +1,13 @@
+const { get } = require('https');
+
+const req = get('https://www.google.com', (res) => {
+    res.on('data', (chunk) => {
+        console.log(`Data Chunk: ${chunk}`);
+    })
+
+    res.on('end', () => {
+        console.log('No more data');
+    })
+})
+
+// req.end()
